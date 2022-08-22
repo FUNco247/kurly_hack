0ba676f8536b196bc666d647609e5970e7368ca9
FUNco <crispy.alc@gmail.com>
2022-08-23 02:02:32 +0900

    ENH Title in Category Page

--- src/components/CategoryPage/OtherProducts.js
+++ src/components/CategoryPage/OtherProducts.js
@@ -1,11 +1,46 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 const Wrapper = styled.div`
   display: grid;
   gap: 8px;
   grid-template-columns: 1fr 1fr;
   padding: 0 16px 0 16px;
+`;
+
+const CategoryName = styled.div`
+  width: 100vw;
+  position: relative;
+  display: block;
+  padding: 32px 16px 0px 16px;
+  div {
+    font-size: 16px;
+    font-weight: 500;
+    color: #333333;
+    display: block;
+    margin: 2px 0px;
+    width: 100%;
+    height: 18px;
+    &:first-child {
+      margin-bottom: 16px;
+    }
+    span {
+      &:first-child {
+        float: left;
+        font-size: 13px;
+        font-weight: 400;
+        color: #333333;
+      }
+      &:last-child {
+        float: right;
+        font-size: 13px;
+        font-weight: 400;
+        color: #333333;
+      }
+    }
+  }
 `;
 
 const ProductBox = styled.div`
@@ -51,33 +86,44 @@ const ProductInfo = styled.div`
 
 function OtherProdunts({ products }) {
   return (
-    <Wrapper>
-      {products.map((product) => (
-        <Link to={`/product/${product.id}`} key={product.id}>
-          <ProductBox>
-            <ProductImg>
-              <img
-                src={
-                  product.fileName
-                    ? `http://3.34.2.197:8080/${product.fileName}`
-                    : "https://t1.daumcdn.net/cfile/tistory/2363B54A560BDA7605"
-                }
-                alt="사진"
-              />
-            </ProductImg>
-            <ProductInfo>
-              <h3>샛별 배송</h3>
-              <h2>{product.name}</h2>
-              <h1>
-                {`${product.price
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`}
-              </h1>
-            </ProductInfo>
-          </ProductBox>
-        </Link>
-      ))}
-    </Wrapper>
+    <>
+      <CategoryName>
+        <div>가격이 더 오르기 전에</div>
+        <div>
+          <span>{`총 ${products.length}개`}</span>
+          <span>
+            추천순 <FontAwesomeIcon icon={faAngleDown} />
+          </span>
+        </div>
+      </CategoryName>
+      <Wrapper>
+        {products.map((product) => (
+          <Link to={`/product/${product.id}`} key={product.id}>
+            <ProductBox>
+              <ProductImg>
+                <img
+                  src={
+                    product.fileName
+                      ? `http://3.34.2.197:8080/${product.fileName}`
+                      : "https://t1.daumcdn.net/cfile/tistory/2363B54A560BDA7605"
+                  }
+                  alt="사진"
+                />
+              </ProductImg>
+              <ProductInfo>
+                <h3>샛별 배송</h3>
+                <h2>{product.name}</h2>
+                <h1>
+                  {`${product.price
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`}
+                </h1>
+              </ProductInfo>
+            </ProductBox>
+          </Link>
+        ))}
+      </Wrapper>
+    </>
   );
 }
 
